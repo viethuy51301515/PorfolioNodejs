ee01f0296da4726ecb5d9609d5570809bb6faaa2
viethuy <viethuy51301515@gmail.com>
2021-02-25 06:53:04 +0700

    update

--- routes/api/achivement.js
+++ routes/api/achivement.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Achivement = require("../../model/Achivement");
 const { body, validationResult } = require("express-validator");
-router.get("/",async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     const achivements = await Achivement.find();
     return res.json(achivements);
@@ -23,12 +23,11 @@ router.post(
   ],
   (req, res) => {
     try {
-        console.log('achivement')
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
         return res.status(401).json({ errors: errors.array() });
       }
-      const achivement = new Achivement({ 
+      const achivement = new Achivement({
         date: req.body.date,
         des: req.body.des,
         img: req.body.img,
